6107d73a86fef1b5c2abf502597482c8d1984765
Adebayo Samuel <eladebayoor@gmail.com>
2023-03-19 01:45:46 -0400

    feat: /accounts/:userId/transactions endpoint exposed
    Signed-off-by: Adebayo Samuel <eladebayoor@gmail.com>

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import createAccount from '../controllers/createAccount';
 import retrieveAccount from '../controllers/retrieveAccount';
+import getAccountTransactions from '../controllers/getAccountTransactions';
 
 const router = Router();
 
 router.post('/accounts', createAccount);
 router.get('/accounts/:userId', retrieveAccount);
+router.get('/accounts/:userId/transactions', getAccountTransactions);
 // router.post('/transactions', TransactionController.postTransaction);
 // router.get('/transactions', TransactionController.getTransactions);
 
